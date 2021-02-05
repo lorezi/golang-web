a724492a486fb378f6a456ac5e96e4d8c62268a5
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-02-05 17:37:58 +0100

    read from a server

--- server/readServer/main.go
+++ server/readServer/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"time"
 )
 
 func main() {
@@ -28,10 +29,15 @@ func main() {
 }
 
 func handle(conn net.Conn) {
+	err := conn.SetDeadline(time.Now().Add(10 * time.Second))
+	if err != nil {
+		log.Println("CONN TIMEOUT")
+	}
 	scanner := bufio.NewScanner(conn)
 	for scanner.Scan() {
 		ln := scanner.Text()
 		fmt.Println(ln)
+		fmt.Fprintf(conn, "I heard you say: %s\n", ln)
 	}
 	defer conn.Close()
 
