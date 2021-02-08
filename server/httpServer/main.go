6981c60ee62a72daf6d1c9b6808344b50c24e089
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-02-08 16:55:39 +0100

    listen and serve http

---
+++ server/httpServer/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+)
+
+func main() {
+	li, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	defer li.Close()
+
+	for {
+		conn, err := li.Accept()
+		if err != nil {
+			log.Fatalln(err.Error())
+		}
+		go handle(conn)
+	}
+}
+
+func handle(conn net.Conn) {
+	defer conn.Close()
+
+	// read request
+	request(conn)
+
+	// write response
+	respond(conn)
+}
+func request(conn net.Conn) {
+	i := 0
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		ln := scanner.Text()
+		fmt.Println(ln)
+		if i == 0 {
+			// request line
+			m := strings.Fields(ln)[0]
+			fmt.Println("****METHOD", m)
+		}
+		if ln == "" {
+			// headers are done
+			break
+		}
+	}
+
+}
+
+func respond(conn net.Conn) {
+
+}
---
+++ server/listenAndServer/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type hotdog int
+
+// http handler
+func (m hotdog) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Any code you want in this func")
+}
+
+func main() {
+	var d hotdog
+
+	http.ListenAndServe(":8080", d)
+}
