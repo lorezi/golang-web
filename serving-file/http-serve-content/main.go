64403d803a3b1931f159ba61eabce066632d71d9
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-02-10 15:54:25 +0100

    http serve content

--- serving-file/http-serve-content/main.go
+++ serving-file/http-serve-content/main.go
@@ -8,22 +8,32 @@ import (
 
 func dog(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	io.WriteString(w, `<img src="serving-file/local-file/dog.jpg"/>`)
+	io.WriteString(w, `<img src="./dog.jpg"/>`)
 }
 
 func dogPic(w http.ResponseWriter, req *http.Request) {
 	f, err := os.Open("serving-file/local-file/dog.jpg")
+
 	if err != nil {
 		http.Error(w, "file not found", 404)
 		return
 	}
+
 	defer f.Close()
 
-	io.Copy(w, f)
+	fi, err := f.Stat()
+	if err != nil {
+		http.Error(w, "file not found", 404)
+		return
+	}
+
+	http.ServeContent(w, req, f.Name(), fi.ModTime(), f)
 }
 
 func main() {
+
 	http.HandleFunc("/", dog)
 	http.HandleFunc("/dog.jpg", dogPic)
 	http.ListenAndServe(":8080", nil)
+
 }
